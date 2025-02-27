e01e3632ed5ed3c6410d61a1e8ca6fa0dae87dca
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-02-27 18:07:18 +0900

    diwje

--- edprompt/edprompt.py
+++ edprompt/edprompt.py
@@ -21,10 +21,11 @@ class edprompt:
 		write_the_text_file(input("save as : "), res)
 	
 	def __pos__(self):
-		ret = _
-		ret = read_the_text_file(ret)
-		print(ret)
-		_ = ret
-		return _
+		global _
+		res = _
+		res = read_the_text_file(res)
+		print(res)
+		_ = res
+		return res
 
 __builtins__.y = edprompt()
