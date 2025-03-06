f37bc93e8f9167b492ab8e9c0c0d18ec07c3e28d
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-03-06 19:40:37 +0900

    umm

--- edprompt/edprompt.py
+++ edprompt/edprompt.py
@@ -2,11 +2,13 @@ import builtins as __builtins__;
 from functools import wraps as _smart_deco_wrap;
 from martialaw.martialaw import partial as _fical
 
+__builtins__.encoding = 'utf-8';
+
 def wither(opener = _fical(open, mode = 'w')):
 	def with_deco(func):
 		@_smart_deco_wrap(func)
 		def with_opener(name, *argv, **kargv):
-			with opener(name) as man:
+			with opener(name, encoding = __builtins__.encoding) as man:
 				return func(man, *argv, **kargv)
 		return with_opener
 	return with_deco
